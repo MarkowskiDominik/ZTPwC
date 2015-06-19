92b848ef96d5be24c7b964b54dbde2c9449d44e8
MarkowskiDominik <domin.1992@gmail.com>
2015-06-19 09:21:29 +0200

    Update Vector.h

--- lab13/Vector.h
+++ lab13/Vector.h
@@ -1,6 +1,6 @@
 //Markowski Dominik
 
-#ifndef VECTOR_H // include guard = stra¿nik do³¹czania
+#ifndef VECTOR_H // include guard = straÂ¿nik doÂ³Â¹czania
 #define VECTOR_H
 
 #include <memory>
@@ -8,57 +8,57 @@
 template<typename T, typename A = std::allocator<T>>
 class Vector {
 public:
-	using value_type = T;										// typ elementu (w ka¿dym kontenerze)
+	using value_type = T;						// typ elementu (w kaÂ¿dym kontenerze)
 	using size_type = typename std::allocator<T>::size_type;	// bezznakowy typ
-																// indeksów kontenera, rozmiaru itp. (np. unsigned int)
+									// indeksÃ³w kontenera, rozmiaru itp. (np. unsigned int)
 
-	using iterator = T*;				// zale¿ny od implementacji
-	using const_iterator = const T*;	// zale¿ny od implementacji
+	using iterator = T*;			// zaleÂ¿ny od implementacji
+	using const_iterator = const T*;	// zaleÂ¿ny od implementacji
 	using reference = value_type&;
 	using allocator_type = A;
 
-	explicit Vector(const A& = A());	// konstruktor domyœlny tworzy wektor pusty
+	explicit Vector(const A& = A());	// konstruktor domyÅ“lny tworzy wektor pusty
 	explicit Vector(size_type count, const T& val = T(), const A& = A());
-	Vector(const Vector& v);			// konstruktor kopiuj¹cy
-	Vector(Vector&& v);					// konstruktor przenosz¹cy
-	explicit Vector(std::initializer_list<T> il, const A& = A()); // konstruktor z list¹ inicjacyjn¹
+	Vector(const Vector& v);		// konstruktor kopiujÂ¹cy
+	Vector(Vector&& v);			// konstruktor przenoszÂ¹cy
+	explicit Vector(std::initializer_list<T> il, const A& = A()); // konstruktor z listÂ¹ inicjacyjnÂ¹
 
 	~Vector();	// destruktor
 
-	Vector& operator=(const Vector& v);				// przypisanie kopiuj¹ce
-	Vector& operator=(Vector&& v);					// przypisanie przenosz¹ce
-	Vector& operator=(std::initializer_list<T> il); // przypisanie z list¹ inicjacyjn¹
+	Vector& operator=(const Vector& v);		// przypisanie kopiujÂ¹ce
+	Vector& operator=(Vector&& v);			// przypisanie przenoszÂ¹ce
+	Vector& operator=(std::initializer_list<T> il); // przypisanie z listÂ¹ inicjacyjnÂ¹
 
 	reference operator[](size_type i) const;	// bez kontroli zakresu
-	reference at(size_type i) const;			// z kontrol¹ zakresu (rzuca wyj¹tek
-												// out_of_range{ "Vector::at()" })
+	reference at(size_type i) const;		// z kontrolÂ¹ zakresu (rzuca wyjÂ¹tek
+							// out_of_range{ "Vector::at()" })
 
-	void swap(Vector& v);	// zamienia elementy aktualnego wektora z wektorem v
+	void swap(Vector& v);		// zamienia elementy aktualnego wektora z wektorem v
 	bool empty() const;		// sprawdza, czy wektor jest pusty
-	size_type capacity() const;	// zwraca najwiêksz¹ mo¿liw¹ liczbê
-								// elementów bez realokacji (pojemnoœæ)
-	size_type size() const;	// zwraca aktualn¹ liczbê elementów (rozmiar)
-	void reserve(size_type newCapacity);			// zwiêksza pojemnoœæ do newCapacity
+	size_type capacity() const;	// zwraca najwiÃªkszÂ¹ moÂ¿liwÂ¹ liczbÃª
+					// elementÃ³w bez realokacji (pojemnoÅ“Ã¦)
+	size_type size() const;				// zwraca aktualnÂ¹ liczbÃª elementÃ³w (rozmiar)
+	void reserve(size_type newCapacity);		// zwiÃªksza pojemnoÅ“Ã¦ do newCapacity
 	void resize(size_type newSize, const T& = {});	// zmienia rozmiar do newSize
 	void shrink_to_fit();
 	void push_back(const T& val);
 	void push_back(T&& val);
 	iterator insert(const_iterator pos, const T& val);
 	iterator insert(const_iterator pos, T&& val);
-	void clear();	// opró¿nia wektor
+	void clear();	// oprÃ³Â¿nia wektor
 
 
-	iterator begin();				// wskazuje na pierwszy element
-	iterator end();					// wskazuje na element pierwszy za ostatnim
+	iterator begin();		// wskazuje na pierwszy element
+	iterator end();			// wskazuje na element pierwszy za ostatnim
 	const_iterator begin() const;
 	const_iterator end() const;
 	const_iterator cbegin() const;	// od C++11
 	const_iterator cend() const;	// od C++11
 
 private:
-	T* elem;	// elem wskazuje tablicê sz elementów typu T
-	T* sz;		// liczba elementów w tablicy
-	T* last;	// liczba elementów + wolna przestrzeñ (bie¿¹cy rozmiar alokacji)
+	T* elem;	// elem wskazuje tablicÃª sz elementÃ³w typu T
+	T* sz;		// liczba elementÃ³w w tablicy
+	T* last;	// liczba elementÃ³w + wolna przestrzeÃ± (bieÂ¿Â¹cy rozmiar alokacji)
 	A alloc;	// alokator
 };
 #endif
